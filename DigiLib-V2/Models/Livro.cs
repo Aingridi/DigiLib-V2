df2c8b04c3cddf7fb78b46224951813f9ac66186
AINGRIDI OLIVEIRA <aingridi.oliveira@fatec.sp.gov.br>
2022-05-21 15:19:09 -0300

    Adicionar arquivos de projeto.

---
+++ DigiLib-V2/Models/Livro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigiLib_V2.Models
+{
+    public class Livro
+    {
+        public int Codigo { get; set; }
+        public int Titulo { get; set; }
+    }
+}
